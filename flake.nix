9d0fc4061cf898efeba13711da392f59d1af2c3c
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-12-23 17:40:18 +0100

    ci(flake): add initial version

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "NAHO's University Bachelor Semester Project 3";
+
+  inputs = {
+    flakeUtils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    preCommitHooks = {
+      inputs = {
+        flake-utils.follows = "flakeUtils";
+        nixpkgs-stable.follows = "preCommitHooks/nixpkgs";
+        nixpkgs.follows = "nixpkgs";
+      };
+
+      url = "github:cachix/pre-commit-hooks.nix";
+    };
+  };
+
+  outputs = {
+    self,
+    flakeUtils,
+    nixpkgs,
+    preCommitHooks,
+    ...
+  }:
+    flakeUtils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        checks.preCommitHooks = preCommitHooks.lib.${system}.run {
+          hooks = {
+            alejandra.enable = true;
+            convco.enable = true;
+            rustfmt.enable = true;
+            typos.enable = true;
+            yamllint.enable = true;
+          };
+
+          settings.alejandra.verbosity = "quiet";
+          src = ./.;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inherit (self.checks.${system}.preCommitHooks) shellHook;
+          packages = with pkgs; [cargo-watch inetutils rustup];
+        };
+      }
+    );
+}
